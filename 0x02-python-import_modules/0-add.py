26eddda2580f5c32d4ae88d93ffbf90d6e92391b
meta-bay <bayiemeseret@gmail.com>
2023-11-02 21:30:21 +0300

    requirement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import add_0 as add
 
-a = 1
-b = 2
-result = add.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add.add(a, b)
+    print("{} + {} = {}".format(a, b, result))
