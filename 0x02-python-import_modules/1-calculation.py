e73e54fc3efb8f5d6ecbe38809861d1cecb455bd
meta-bay <bayiemeseret@gmail.com>
2023-11-02 21:49:07 +0300

    i think it works

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import calculator_1 as calc
+    a = 10
+    b = 5
+    sum = calc.add(a, b)
+    sub = calc.sub(a, b)
+    mul = calc.mul(a, b)
+    div = calc.div(a, b)
+
+    print("{} + {} = {}".format(a, b, sum))
+    print("{} - {} = {}".format(a, b, sub))
+    print("{} * {} = {}".format(a, b, mul))
+    print("{} / {} = {}".format(a, b, div))
