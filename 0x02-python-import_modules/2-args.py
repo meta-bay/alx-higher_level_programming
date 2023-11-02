863a650235df3f1423d6106f100298b691f6ac3c
meta-bay <bayiemeseret@gmail.com>
2023-11-02 23:07:21 +0300

    just work

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     import sys
     length = len(sys.argv)
     if length < 2:
-        print("{}: arguments.".format(length - 1))
+        print("{} arguments.".format(length - 1))
     if length == 2:
-        print("{}: argument:".format(length - 1))
+        print("{} argument:".format(length - 1))
         print("{}: {}".format(1, sys.argv[1]))
     if length >= 3:
-        print("{}: arguments:".format(length - 1))
+        print("{} arguments:".format(length - 1))
         for i in range(1, length):
             print("{}: {}".format(i, sys.argv[i]))
