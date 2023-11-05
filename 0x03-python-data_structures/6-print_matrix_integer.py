9d668a838dc38915f003d1a6aa1eca0d475f52c3
meta-bay <bayiemeseret@gmail.com>
2023-11-05 17:30:12 +0300

    committing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,11 @@
 
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print("$")
+        print()
     else:
         for row in matrix:
             for i in range(len(row)):
-                print(row[i], end="")
+                print("{:d}".format(row[i]), end="")
                 if i < len(row) - 1:
                     print(" ", end="")
             print("$")
