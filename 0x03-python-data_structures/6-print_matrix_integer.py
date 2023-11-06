4cb4bf54d4c639ff67c94cc7a694d5c763e5c93b
meta-bay <bayiemeseret@gmail.com>
2023-11-06 12:05:48 +0300

    committing

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+    if len(matrix) == 0:
+        print()
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end=" ")
         print()
