02922f17850c73e563fd372c9f2ab7b38c652a1c
meta-bay <bayiemeseret@gmail.com>
2023-11-06 04:08:26 +0300

    committing

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if sentence is None:
+        first = None
+    else:
+        first = sentence[0]
+        length = len(sentence)
+    return (length, first)
