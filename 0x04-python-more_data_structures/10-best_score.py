99b1bc5d4e46d2001405faf5cd2cb49684f9c2e1
meta-bay <bayiemeseret@gmail.com>
2023-11-08 15:29:53 +0300

    committing

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,6 +5,8 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     for key, value in a_dictionary.items():
+        if value is None:
+            return None
         if max_val is None or value > max_val:
             max_val = value
             best_key = key
