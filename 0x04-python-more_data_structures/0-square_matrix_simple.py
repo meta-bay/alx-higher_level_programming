aab2af6d1a3b7424566d6db438562822bba81eb4
meta-bay <bayiemeseret@gmail.com>
2023-11-08 13:41:28 +0300

    removed test code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -11,14 +11,3 @@ def square_matrix_simple(matrix=[]):
         for j in range(len(copy_matrix[i])):
             copy_matrix[i][j] = copy_matrix[i][j] ** 2
     return copy_matrix
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
