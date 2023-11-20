1e8f023365248a99586898e382279db5353df6cb
meta-bay <bayiemeseret@gmail.com>
2023-11-20 23:22:08 +0300

    work

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,12 +1,10 @@
 #include <Python.h>
-#include <string>
-#include <unistd.h>
-#include <stdlib.h>
-#include <float.h>
 #include <object.h>
 #include <listobject.h>
 #include <bytesobject.h>
 #include <floatobject.h>
+#include <stdlib.h>
+#include <float.h>
 
 /**
  * print_python_list - python lists info
