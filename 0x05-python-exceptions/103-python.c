2e4fed334d6c57fa3f9cbbdebba3ba253da7d12d
meta-bay <bayiemeseret@gmail.com>
2023-11-21 01:58:03 +0300

    does it work

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -5,39 +5,6 @@ void print_python_bytes(PyObject *p);
 void print_python_float(PyObject *p);
 
 /**
- * print_python_list - python list info
- * @p: the pyobject
- */
-void print_python_list(PyObject *p)
-{
-	Py_ssize_t size, allocation, i;
-	const char *the_type;
-	PyListObject *the_list = (PyListObject *)p;
-	PyVarObject *the_var = (PyVarObject *)p;
-
-	size = the_var->ob_size;
-	allocation = the_list->allocated;
-	fflush(stdout);
-	printf("[*] Python list info\n");
-	if (strcmp(p->ob_type->tp_name, "list") != 0)
-	{
-		printf("  [ERROR] Invalid List Object\n");
-		return;
-	}
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", allocation);
-	for (i = 0; i < size; i++)
-	{
-		the_type = the_list->ob_item[i]->ob_type->tp_name;
-		printf("Element %ld: %s\n", i, the_type);
-		if (strcmp(the_type, "bytes") == 0)
-			print_python_bytes(the_list->ob_item[i]);
-		else if (strcmp(the_type, "float") == 0)
-			print_python_float(the_list->ob_item[i]);
-	}
-}
-
-/**
  * print_python_bytes - Python byte info
  * @p: the pyobject
  */
@@ -93,4 +60,38 @@ void print_python_float(PyObject *p)
 			Py_DTSF_ADD_DOT_0, NULL);
 	printf("  value: %s\n", buff);
 	PyMem_Free(buff);
+}
+
+/**
+ * print_python_list - python list info
+ * @p: the pyobject
+ */
+
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size, allocation, i;
+	const char *the_type;
+	PyListObject *the_list = (PyListObject *)p;
+	PyVarObject *the_var = (PyVarObject *)p;
+
+	size = the_var->ob_size;
+	allocation = the_list->allocated;
+	fflush(stdout);
+	printf("[*] Python list info\n");
+	if (strcmp(p->ob_type->tp_name, "list") != 0)
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", allocation);
+	for (i = 0; i < size; i++)
+	{
+		the_type = the_list->ob_item[i]->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, the_type);
+		if (strcmp(the_type, "bytes") == 0)
+			print_python_bytes(the_list->ob_item[i]);
+		else if (strcmp(the_type, "float") == 0)
+			print_python_float(the_list->ob_item[i]);
+	}
 }
