68fe2382838ac6db917780a5b3d06847447ee5e8
meta-bay <bayiemeseret@gmail.com>
2023-11-22 01:25:57 +0300

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Constructor
+        Args:
+            size: length square.
+        """
+        self.__size = size
