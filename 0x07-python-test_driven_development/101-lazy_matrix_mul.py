71facf27d29ce76fac78f22ea3f3674579f64a92
meta-bay <bayiemeseret@gmail.com>
2023-11-27 18:19:49 +0300

    little fix on numpy

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,11 +2,17 @@
 '''
     lazy matrix multiplication with numpy
 '''
-from numpy import matmul
+import numpy
 
 
 def lazy_matrix_mul(m_a, m_b):
     '''
         multiplying matrix using numpy
     '''
-    return matmul(m_a, m_b)
+    try:
+        result = numpy.matmul(m_a, m_b)
+        return result
+    except ValueError as e:
+        raise ValueError("Matrices cannot be multiplied") from e
+    except TypeError as e:
+        raise TypeError("The matrices must have valid input") from e
