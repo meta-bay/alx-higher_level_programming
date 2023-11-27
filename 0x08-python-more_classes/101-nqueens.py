0728c873836de643882b5c33f5ee765673b34df6
meta-bay <bayiemeseret@gmail.com>
2023-11-27 20:41:48 +0300

    what about now

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -6,15 +6,15 @@ import sys
 
 
 if len(sys.argv) != 2:
-    print("Usage: nqueens N\n")
+    print("Usage: nqueens N")
     sys.exit(1)
 N = sys.argv[1]
 if not N.isdigit():
-    print("N must be a number\n")
+    print("N must be a number")
     sys.exit(1)
 N = int(N)
 if N < 4:
-    print("N must be at least 4\n")
+    print("N must be at least 4")
     sys.exit(1)
 
 
