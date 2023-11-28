90d0c1f312735377472381618f7878865f9e666a
meta-bay <bayiemeseret@gmail.com>
2023-11-28 18:09:49 +0300

    locked except first_name

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class LockedClass:
     '''
-        prevents the user from 
+        prevents the user from
         dynamically creating new instance attributes,
         except for first_name
     '''
