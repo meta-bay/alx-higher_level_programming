fdf24efa769831533ddc12d57939040c0344a66d
meta-bay <bayiemeseret@gmail.com>
2023-12-06 00:46:43 +0300

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,9 +3,10 @@
     Pascal's Triangle
 '''
 
+
 def generate_pascal_triangle(n):
     '''
-        returns a list of lists of integers 
+        returns a list of lists of integers
         representing the Pascal's triangle of n
     '''
     if n <= 0:
@@ -15,7 +16,8 @@ def generate_pascal_triangle(n):
 
     for i in range(1, n):
         prev_row = p_triangle[-1]
-        new_row = [1] + [prev_row[j] + prev_row[j + 1] for j in range(len(prev_row) - 1)] + [1]
+        new_row = [1] + [prev_row[j] + prev_row[j + 1]
+                         for j in range(len(prev_row) - 1)] + [1]
         p_triangle.append(new_row)
 
     return p_triangle
