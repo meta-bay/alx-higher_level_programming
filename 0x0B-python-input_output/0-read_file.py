3e4929ff6f56108fd38a43582e28deebe45356de
meta-bay <bayiemeseret@gmail.com>
2023-12-05 22:37:58 +0300

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+'''
+    file reading
+'''
+
+
 def read_file(filename=""):
     '''
-        a funciton that reads a file 
+        a funciton that reads a file
     '''
     with open('filename', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
-
