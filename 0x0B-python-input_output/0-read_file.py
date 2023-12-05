60994e39651356baa5a80bb8ffcfbb8fa554ea79
meta-bay <bayiemeseret@gmail.com>
2023-12-05 22:56:01 +0300

    it worked

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     '''
         a funciton that reads a file
     '''
-    with open(filename, mode="r", encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data)
+    with open(filename, encoding="utf-8") as f:
+        for i in f:
+            print(i)
