0f34106fe6c9a9ef9047516ff40ef8dfb6dea314
meta-bay <bayiemeseret@gmail.com>
2023-12-11 19:57:30 +0300

    i think it worked

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,7 +24,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         ''' Returns the JSON string representation of list_dictionaries '''
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
