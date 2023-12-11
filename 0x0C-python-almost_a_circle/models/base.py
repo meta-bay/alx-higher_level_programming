aa1a5635f88f052100e320f6fa097dc5519dd060
meta-bay <bayiemeseret@gmail.com>
2023-12-11 19:54:01 +0300

    it didn't work

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
         ''' Returns the JSON string representation of list_dictionaries '''
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dump(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
