5ecdc67599a522264df48e1d62d8ac0f9982c854
meta-bay <bayiemeseret@gmail.com>
2023-12-12 05:22:38 +0300

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,26 +9,32 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     ''' Base Testing class '''
 
-    def test_id(self):
-        ''' Test id 1'''
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
+    def setUp(self):
+        self.b1 = Base()
+        self.b2 = Base()
+        self.b3 = Base()
+        self.b4 = Base(25)
+        self.b5 = Base()
 
-    def test_id2(self):
-        ''' test id 2'''
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
+    def testBase(self):
+        '''Testing the base class'''
+        id = self.b1.id
+        self.assertEqual(id, 1)
+        id = self.b2.id
+        self.assertEqual(id, 2)
+        id = self.b3.id
+        self.assertEqual(id, 3)
+        id = self.b4.id
+        self.assertEqual(id, 25)
+        id = self.b5.id
+        self.assertEqual(id, 4)
 
-    def test_id3(self):
-        ''' test id 3'''
-        b3 = Base()
-        self.assertEqual(b3.id, 3)
-
-    def test_rand(self):
-        ''' test random id'''
-        b4 = Base(24)
-        self.assertEqual(b4.id, 24)
-
+    def tearDown(self):
+        self.b1.id = None
+        self.b2.id = None
+        self.b3.id = None
+        self.b4.id = None
+        self.b5.id = None
 
 if __name__ == '__main__':
     unittest.main()
