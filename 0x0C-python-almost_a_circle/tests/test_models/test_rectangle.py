c7cbc1651e023e1dc96862efe60b2b86be517f58
meta-bay <bayiemeseret@gmail.com>
2023-12-12 05:48:11 +0300

    update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -15,11 +15,56 @@ class TestRectangle(unittest.TestCase):
 
     def test_rect(self):
         ''' test rectangle'''
-        r1 = Rectangle(10, 20)
-        self.assertEqual(r1.id, 1)
+        r1 = Rectangle(1, 2)
+        r2 = Rectangle(1, 2, 3, 4, 5)
+        r3 = Rectangle(2, 1)
+        r4 = Rectangle(5, 6, 7, 8, 9)
+        r5 = Rectangle(2, 1, 1, 1)
+        r6 = Rectangle(3, 4, 5)
 
-        r2 = Rectangle(20, 10)
-        self.assertEqual(r2.id, 2)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 5)
+
+        with self.assertRaises(TypeError):
+            Rectangle("five", 5)
+        with self.assertRaises(TypeError):
+            Rectangle([10], 2)
+        with self.assertRaises(TypeError):
+            Rectangle((1, 2), 5)
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, [3])
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, (3, 4))
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, {})
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3.5)
+        with self.assertRaises(TypeError):
+            Rectangle({}, 5)
+        with self.assertRaises(TypeError):
+            Rectangle(1.5, 5)
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, '4')
+        with self.assertRaises(TypeError):
+            Rectangle(1, 3, 4, [5])
+        with self.assertRaises(TypeError):
+            Rectangle(1, 3, 4, (5, 2))
+        with self.assertRaises(TypeError):
+            Rectangle(1, 5, 6, {})
+        with self.assertRaises(TypeError):
+            Rectangle(1, 5, 6, 7.5)
+        with self.assertRaises(TypeError):
+            Rectangle(106, '3')
+        with self.assertRaises(TypeError):
+            Rectangle(1, [2])
+        with self.assertRaises(TypeError):
+            Rectangle(1, (2, 3))
+        with self.assertRaises(TypeError):
+            Rectangle(1, {})
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2.5)
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, '3')
 
     def test_something(self):
         ''' test something '''
