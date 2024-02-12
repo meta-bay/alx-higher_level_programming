02a29f7a377093e6ad74acbec698d36fa2c09a08
meta-bay <bayiemeseret@gmail.com>
2024-02-13 02:12:52 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (num === 1 || isNaN(num)) {
+    return 1;
+  }
+  return (num * factorial(num - 1));
+}
+const num = Number(process.argv[2]);
+console.log(factorial(num));
