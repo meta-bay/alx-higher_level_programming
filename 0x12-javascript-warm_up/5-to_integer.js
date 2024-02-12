6f8eecf7b574dbb11dc6e949f5a2b4577bb890de
meta-bay <bayiemeseret@gmail.com>
2024-02-13 01:42:47 +0300

    semistandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,7 @@
 
 const { argv } = require('process');
 if (isNaN(argv[2])) {
-  console.log('Not a number')
-}
-else {
-    console.log('My number:', Number(argv[2]));
+  console.log('Not a number');
+} else {
+  console.log('My number:', Number(argv[2]));
 }
