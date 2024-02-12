3da9b8bf1f76e9ad1e17d6d61093aa2827d357d1
meta-bay <bayiemeseret@gmail.com>
2024-02-13 02:15:40 +0300

    nothing

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,5 +7,5 @@ function factorial (num) {
   return (num * factorial(num - 1));
 }
 const num = Number(process.argv[2]);
-const the_factorial = factorial(num);
-console.log(the_factorial);
+const theFactorial = factorial(num);
+console.log(theFactorial);
