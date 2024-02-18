c7c58c2f92542ee755428b23ca9de0429dc7140d
meta-bay <bayiemeseret@gmail.com>
2024-02-18 13:41:52 +0300

    get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
-    curs = conn.cursor()
+    curs = db.cursor()
 
     curs.execute('SELECT * FROM states ORDER BY id ASC')
     states = curs.fetchall()
