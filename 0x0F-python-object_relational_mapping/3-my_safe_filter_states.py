d2b4b7266bbd681c91458ee21bf40c8cae5a9216
meta-bay <bayiemeseret@gmail.com>
2024-02-19 03:24:30 +0300

    sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+    safe filter states
+'''
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    curs = db.cursor()
+    curs.execute('SELECT * FROM states WHERE name \
+            LIKE BINARY %s', (sys.argv[4],))
+    states = curs.fetchall()
+    for state in states:
+        print(state)
+    curs.close()
+    db.close()
