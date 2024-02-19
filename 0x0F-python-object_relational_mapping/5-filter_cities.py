0fba70d11c055a0d535c318b12d5f09b2a3a392e
meta-bay <bayiemeseret@gmail.com>
2024-02-19 05:52:07 +0300

    fix pycodestyle

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
     sql = 'SELECT cites.name FROM cities
-            INNER JOIN states ON cities.state_id = states.id
-            WHERE states.name = %s
-            ORDER BY cites.id ASC'
+    INNER JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cites.id ASC'
     curs.execute(sql, (sys.argv[4],))
     cities = curs.fetchall()
     city_in_s = ", ".join(city[0] for city in cities)
