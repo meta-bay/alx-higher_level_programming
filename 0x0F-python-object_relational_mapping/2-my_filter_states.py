d25f811cf6a610dcf675371198093fe322b1ba80
meta-bay <bayiemeseret@gmail.com>
2024-02-19 03:04:40 +0300

    filster states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+    filter states by name
+'''
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db = sys.argv[3])
+    curs = db.cursor()
+    curs.execute('SELECT * FROM states WHERE name LIKE BINARY "{}"'.format(sys.argv[4]))
+    states = curs.fetchall()
+    for state in states:
+        print(state)
+    curs.close()
+    db.close()
+    
