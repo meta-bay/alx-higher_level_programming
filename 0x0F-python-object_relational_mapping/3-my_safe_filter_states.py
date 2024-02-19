563ee30fcf0ed37ca7feb980a8f91815a2cdddfd
meta-bay <bayiemeseret@gmail.com>
2024-02-19 03:25:53 +0300

    nothing

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
     curs.execute('SELECT * FROM states WHERE name \
-            LIKE BINARY %s', (sys.argv[4],))
+            LIKE BINARY %s', sys.argv[4])
     states = curs.fetchall()
     for state in states:
         print(state)
