5f6a7ccf5d942c9d0e17d563a2e139159b185681
meta-bay <bayiemeseret@gmail.com>
2024-02-19 05:54:20 +0300

    nothing

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    sql = '''SELECT cites.name FROM cities 
-    INNER JOIN states ON cities.state_id = states.id 
+    sql = '''SELECT cites.name FROM cities
+    INNER JOIN states ON cities.state_id = states.id
     WHERE states.name = %s ORDER BY cites.id ASC'''
     curs.execute(sql, (sys.argv[4],))
     cities = curs.fetchall()
