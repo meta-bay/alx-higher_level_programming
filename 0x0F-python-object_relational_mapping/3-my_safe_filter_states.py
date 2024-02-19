07b832de35f031fceddbeccd55f3867227105a53
meta-bay <bayiemeseret@gmail.com>
2024-02-19 04:52:14 +0300

    fetch all model state

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    sql = 'SELECT * FROM states WHERE name LIKE BINARY %s')
+    sql = 'SELECT * FROM states WHERE name LIKE BINARY %s'
     curs.execute(sql, (sys.argv[4],))
     states = curs.fetchall()
     for state in states:
