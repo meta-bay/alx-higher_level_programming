76192b1c53be2493405d836c46ca7d32fd2be6b9
meta-bay <bayiemeseret@gmail.com>
2024-02-19 04:24:24 +0300

    fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-    safe filter states
+    cities in the state
 '''
 
 
@@ -11,10 +11,4 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    sql = 'SELECT * FROM states WHERE name LIKE BINARY %s')
-    curs.execute(sql, (sys.argv[4],))
-    states = curs.fetchall()
-    for state in states:
-        print(state)
-    curs.close()
-    db.close()
+
