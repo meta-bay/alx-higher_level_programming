8d0122a46a32683aa8432e56d988b0cd61d406ec
meta-bay <bayiemeseret@gmail.com>
2024-03-01 02:10:46 +0300

    nothing

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # curl body size
-
-content_length=$(curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f 2)
-echo "$content_length"
-
+echo curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f 2
