49cd00322c89ac5bb5843016d95dd4510b589dfc
meta-bay <bayiemeseret@gmail.com>
2024-03-02 01:09:08 +0300

    header id using requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+    X-Request-Id
+'''
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    req_id = req.headers['X-Request-Id']
+    print(req_id)
