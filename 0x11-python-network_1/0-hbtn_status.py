5d4b6c081f46df298833ac4ef0fe13e529e8332c
meta-bay <bayiemeseret@gmail.com>
2024-03-01 17:45:50 +0300

    just work

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         body = r.read()
-        print('Body response:')
-        print(' - type: ', type(body))
-        print(' - content: ', body)
-        print(' - utf8 content: ', body.decode('utf8'))
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
