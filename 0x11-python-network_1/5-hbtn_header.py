24dafe9d78f8f2e7b2a383dd253a65459cb0db9b
meta-bay <bayiemeseret@gmail.com>
2024-03-02 01:13:10 +0300

    it worked

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,5 +9,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
-    req_id = req.headers['X-Request-Id']
+    req_id = req.headers.get('X-Request-Id')
     print(req_id)
