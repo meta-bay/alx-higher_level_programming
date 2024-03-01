72cf8166f06fe1ab7a450d0645674c9a42f749e5
meta-bay <bayiemeseret@gmail.com>
2024-03-01 23:45:11 +0300

    response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,6 +7,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        req_id = response.headers.get('X-Requested-ID')
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        req_id = response.headers.get('X-Request-Id')
         print(req_id)
