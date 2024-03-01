4f6acb77561b5cc69b9f4903fbe4849058574338
meta-bay <bayiemeseret@gmail.com>
2024-03-01 17:42:50 +0300

    what's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import urllib.request
+'''
+    script that fetches https://alx-intranet.hbtn.io/status
+'''
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        body = r.read()
+        print('Body response:')
+        print(' - type: ', type(body))
+        print(' - content: ', body)
+        print(' - utf8 content: ', body.decode('utf8'))
