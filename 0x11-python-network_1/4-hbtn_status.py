f58caa6d9c29961c8210a75e83284d27aa57e1ef
meta-bay <bayiemeseret@gmail.com>
2024-03-02 00:55:39 +0300

    my status using requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+	my status
+'''
+import requests
+
+
+if __name__ == "__main__":
+	req = requests.get('https://alx-intranet.hbtn.io/status')
+	print('Body response:')
+	print('\t- type:', type(req.text))
+	print('\t- content:', req.text)
