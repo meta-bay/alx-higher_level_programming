9ca8d23278f21e969ba92990749bc1d18ebc2b4a
meta-bay <bayiemeseret@gmail.com>
2024-03-02 01:55:19 +0300

    error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+    Error code
+'''
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code == requests.code.ok:
+        print(response.text)
+    else:
+        print(response.status_code)
