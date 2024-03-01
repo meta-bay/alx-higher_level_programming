6f96918930dfca151ac36cb67be713676fb8e91e
meta-bay <bayiemeseret@gmail.com>
2024-03-02 02:42:32 +0300

    nah

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,19 +2,20 @@
 '''
     interview
 '''
-from requests
-from sys
+import requests
+import sys
 
 
 if __name__ == "__main__":
     repo = sys.argv[1]
     username = sys.argv[2]
     url = 'https://api.github.com/repos/{}/{}/commits'.format(username, repo)
-    commits = get(url).json()
+    commits = requests.get(url).json()
     try:
         for i in range(10):
             print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+                commits[i].requests.get("sha"),
+                commits[i].requests.get("commit").
+                requests.get("author").requests.get("name")))
     except Exception:
         pass
