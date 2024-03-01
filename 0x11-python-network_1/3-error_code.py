fac0d47527e4aff54416a6ff4e3c4e4f005420e9
meta-bay <bayiemeseret@gmail.com>
2024-03-02 00:34:37 +0300

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 '''
-	Error code
+    Error code
 '''
 import sys
 import urllib.request
 
 
 if __name__ == "__main__":
-	req = urllib.request.Request(sys.argv[1])
-	try:
-		urllib.request.urlopen(req)
-	except urllib.error.HTTPError as e:
-		print('Error code: ', e.code)
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        urllib.request.urlopen(req)
+    except urllib.error.HTTPError as e:
+        print('Error code: ', e.code)
