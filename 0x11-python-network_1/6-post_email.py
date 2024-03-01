ea9afa93d7ac6a8260f92861249011be254654c4
meta-bay <bayiemeseret@gmail.com>
2024-03-02 01:32:12 +0300

    just work

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,6 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    response = requests.post(url, data={'email': email})
+    value = {'email': email}
+    response = requests.post(url, data=value)
     print(response.text)
