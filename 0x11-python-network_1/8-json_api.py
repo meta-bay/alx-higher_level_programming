4e8e9cc7e90df7b2713ceadc9b05ce51ad96aa1f
meta-bay <bayiemeseret@gmail.com>
2024-03-02 02:23:47 +0300

    searc api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,11 @@ import sys
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
-    q = sys.argv[1]
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
     response = requests.post(url, {'q': q})
     try:
         data = response.json()
