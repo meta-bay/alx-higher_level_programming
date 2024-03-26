d71cbc3dba345bfb82aa5c1f1e4606676871a73a
meta-bay <bayiemeseret@gmail.com>
2024-03-27 01:15:31 +0300

    right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,15 +4,18 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request(url, (err, response, body) => {
+request(url, async (err, response, body) => {
   if (err) { console.error(err); }
   const result = JSON.parse(body);
   const characters = result.characters;
-  characters.forEach(charUrl => {
-    request(charUrl, (e, r, body) => {
-      if (e) { console.error(e); }
-      const char = JSON.parse(body);
-      console.log(char.name);
+  for (const charLink of characters) {
+    await new Promise((resolve, reject) => {
+      request(charLink, (e, r, body) => {
+        if (e) { reject(e); }
+        const theChar = JSON.parse(body);
+        console.log(theChar.name);
+        resolve();
+      });
     });
-  });
+  }
 });
