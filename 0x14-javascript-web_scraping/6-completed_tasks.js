8674b5e984606ed432b73bb0f72523f4de64c3d8
meta-bay <bayiemeseret@gmail.com>
2024-03-27 00:22:58 +0300

    how many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,19 +4,19 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, response, body) => {
-    if(err){
-        console.error(err);
+  if (err) {
+    console.error(err);
+  }
+  const completedUserTask = {};
+  const todos = JSON.parse(body);
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedUserTask[todo.userId]) {
+        completedUserTask[todo.userId]++;
+      } else {
+        completedUserTask[todo.userId] = 1;
+      }
     }
-    const completedUserTask = {};
-    todos = JSON.parse(body)
-    todos.forEach(todo => {
-        if(todo.completed){
-            if(completedUserTask[todo.userId]){
-                completedUserTask[todo.userId]++;
-            } else {
-                completedUserTask[todo.userId] = 1;
-            }
-        }
-    });
-    console.log(completedUserTask);
+  });
+  console.log(completedUserTask);
 });
