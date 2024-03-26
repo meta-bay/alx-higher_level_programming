de470cb89ac580b1e64e33f4f33da6a820cae5fb
meta-bay <bayiemeseret@gmail.com>
2024-03-26 16:23:16 +0300

    semistandard fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2]
+const url = process.argv[2];
 
 request
-.get(url)
-.on('response', function (response) {
-    console.log('code:', response.statusCode)
-})
+  .get(url)
+  .on('response', function (response) {
+    console.log('code:', response.statusCode);
+  });
