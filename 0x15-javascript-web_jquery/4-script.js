f1c710cf778582ba82e92251ce629784a00411f2
meta-bay <bayiemeseret@gmail.com>
2024-04-03 17:49:16 +0300

    use toggleClass()

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,9 +1,5 @@
 const header = $('header');
 const toggleHeader = $('DIV#toggle_header');
 toggleHeader.on('click', () => {
-  if (header.hasClass('red')) {
-    header.removeClass('red').addClass('green');
-  } else {
-    header.removeClass('green').addClass('red');
-  }
+    header.toggleClass('red green');
 });
