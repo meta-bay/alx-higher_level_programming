282a6bac2c76f6bcd2f77139581030866c21b7f4
meta-bay <bayiemeseret@gmail.com>
2024-04-04 05:27:33 +0300

    list, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  $('DIV#add_item').on('click', () => {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').on('click', () => {
+    $('UL.my_list li:last-child').remove();
+  });
+  $('DIV#clear_list').on('click', () => {
+    $('UL.my_list').empty();
+  });
+});
